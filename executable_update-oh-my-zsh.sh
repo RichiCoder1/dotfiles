d832175b3882f086f845f6ea62b666014c47bd36
Richard Simpson <richardsimpson@outlook.com>
2020-05-28 16:18:29 -0500

    update oh-my-zsh

--- executable_update-oh-my-zsh.sh
+++ executable_update-oh-my-zsh.sh
@@ -1,2 +1,3 @@
 curl -s -L -o oh-my-zsh-master.tar.gz https://github.com/robbyrussell/oh-my-zsh/archive/master.tar.gz
 chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh oh-my-zsh-master.tar.gz
+rm oh-my-zsh-master.tar.gz
