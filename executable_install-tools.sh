460d89c467f875984dd6962a4b277be650037a95
Richard Simpson <richardsimpson@outlook.com>
2020-05-28 17:45:20 -0500

    clean up installer

--- executable_install-tools.sh
+++ executable_install-tools.sh
@@ -1,8 +1,16 @@
 #!/bin/zsh
 
 echo "Upgrading system packages..."
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common libssl-dev libffi-dev git wget nano
+sudo apt-get update -qq
+sudo apt-get install -yqq apt-transport-https ca-certificates curl software-properties-common libssl-dev libffi-dev git wget nano gpg
+
+echo "Upgrading dotnet..."
+curl -sSL -o- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o- | sudo apt-key add -
+sudo curl -sSL https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list -o /etc/apt/sources.list.d/microsoft-prod.list 
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+sudo apt-get update -qq
+sudo apt-get install dotnet-sdk-3.1 -yqq
 
 echo "Upgrading go..."
 GO_VERSION=1.14.3
@@ -15,34 +23,26 @@ curl -sSL https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz -o /tmp/go.t
     sudo tar -C /usr/local -xzf /tmp/go.tar.gz && \
     rm /tmp/go.tar.gz
 
-echo "Upgrading dotnet..."
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current && \
-    mkdir -p ~/bin
-
-if [[ ! -L $HOME/bin/dotnet ]] ; then
-    sudo ln -s ~/.dotnet/dotnet ~/bin/dotnet
-fi
-
 echo "Upgrading nvm..."
 NVM_DIR="$HOME/.nvm"
 if [[ -d $NVM_DIR ]] ; then 
     rm -rf $NVM_DIR
 fi
 (
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  git clone -q https://github.com/nvm-sh/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` -q
 ) && \. "$NVM_DIR/nvm.sh"
 nvm install node
 
-echo "Install kubectl Repo..."
+echo "Install kubectl repo..."
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 echo "Updating and cleaning packages"
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get autoremove -y
+sudo apt-get update -qq
+sudo apt-get upgrade -yqq
+sudo apt-get autoremove -yqq
 
 echo "Installing kubectl"
-sudo apt-get install -y kubectl
+sudo apt-get install -yqq kubectl
